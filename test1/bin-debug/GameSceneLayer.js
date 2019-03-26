1d0afd2db88bf7863ea9002c72ce992077a99585
MengjiaYao <MengjiaYao@boyaa.com>
2019-03-26 16:02:28 +0800

    更新简易版剪刀布

--- test1/bin-debug/GameSceneLayer.js
+++ test1/bin-debug/GameSceneLayer.js
@@ -21,6 +21,33 @@ var GameSceneLayer = (function (_super) {
         this.addChild(sky);
         sky.width = GameApp.stageW;
         sky.height = GameApp.stageH;
+        var DsLabel = new egret.TextField();
+        DsLabel.textColor = 0xffffff;
+        DsLabel.x = GameApp.stageW / 2 - 200;
+        DsLabel.y = GameApp.stageH / 2 - 300;
+        DsLabel.text = "你的对手已经想好要出剪刀了";
+        DsLabel.size = 30;
+        this.addChild(DsLabel);
+        this.DsLabel = DsLabel; //这样写了才能在下面使用visible
+        // this.DsLabel.visible = true;
+        var xtqtImg = this.createBitmapByName("quantou_png");
+        this.addChild(xtqtImg);
+        xtqtImg.x = GameApp.stageW / 2 - 60;
+        xtqtImg.y = GameApp.stageH / 2 - 300;
+        this.xtqtImg = xtqtImg;
+        this.xtqtImg.visible = false;
+        var xtjdImg = this.createBitmapByName("jiandao_png");
+        this.addChild(xtjdImg);
+        xtjdImg.x = GameApp.stageW / 2 - 60;
+        xtjdImg.y = GameApp.stageH / 2 - 300;
+        this.xtjdImg = xtjdImg;
+        this.xtjdImg.visible = false;
+        var xtbuImg = this.createBitmapByName("bazhang_png");
+        this.addChild(xtbuImg);
+        xtbuImg.x = GameApp.stageW / 2 - 60;
+        xtbuImg.y = GameApp.stageH / 2 - 300;
+        this.xtbuImg = xtbuImg;
+        this.xtbuImg.visible = false;
         // var btn:egret.DisplayObject = new egret.DisplayObject();
         var qtImg = this.createBitmapByName("quantou_png");
         this.addChild(qtImg);
@@ -76,24 +103,102 @@ var GameSceneLayer = (function (_super) {
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.qtImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(1);
     };
     GameSceneLayer.prototype.onjdButtonClick = function () {
         this.stLabel.visible = false;
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.jdImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(2);
     };
     GameSceneLayer.prototype.onbuButtonClick = function () {
         this.stLabel.visible = false;
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.buImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(3);
     };
     GameSceneLayer.prototype.createBitmapByName = function (name) {
         var result = new egret.Bitmap();
         var texture = RES.getRes(name);
         result.texture = texture;
         return result;
+    };
+    GameSceneLayer.prototype.getRandomIntInclusive = function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+    };
+    GameSceneLayer.prototype.createPanel = function () {
+        var panel = new eui.Panel();
+        panel.title = "Title";
+        panel.x = 100;
+        panel.y = 500;
+        this.addChild(panel);
+        var strResuletLabel = new egret.TextField();
+        strResuletLabel.textColor = 0x00CED1;
+        strResuletLabel.x = 100;
+        strResuletLabel.y = GameApp.stageH / 2;
+        strResuletLabel.size = 30;
+        this.addChild(strResuletLabel);
+        this.strResuletLabel = strResuletLabel; //这样写了才能在下面使用visible
+        this.strResuletLabel.text = "平局";
+        strResuletLabel.touchEnabled = true; //bitmap设置之后才能触发点击
+        strResuletLabel.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+    };
+    GameSceneLayer.prototype.getRandom = function (result) {
+        var num = this.getRandomIntInclusive(1, 3);
+        console.log(num);
+        if (num === 1) {
+            this.xtqtImg.visible = true;
+        }
+        else if (num === 2) {
+            this.xtjdImg.visible = true;
+        }
+        else if (num === 3) {
+            this.xtbuImg.visible = true;
+        }
+        this.createPanel();
+        if (result === 1) {
+            if (num === 1) {
+            }
+            else if (num === 2) {
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //赢了
+            }
+            else if (num === 3) {
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //输了
+            }
+        }
+        if (result === 2) {
+            if (num === 1) {
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //输了
+            }
+            else if (num === 2) {
+            }
+            else if (num === 3) {
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //赢了
+            }
+        }
+        if (result === 3) {
+            if (num === 1) {
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //赢了
+            }
+            else if (num === 2) {
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊";
+                //输了
+            }
+            else if (num === 3) {
+            }
+        }
     };
     return GameSceneLayer;
 }(egret.Sprite));
--- test1/src/GameSceneLayer.ts
+++ test1/src/GameSceneLayer.ts
@@ -4,18 +4,53 @@ class GameSceneLayer extends egret.Sprite{
         super();//获取最近的一个父类
         this.init();
     }
+    private DsLabel:egret.TextField;//你的对手
     private stLabel;
     private jdLabel;
     private buLabel;
     private qtImg:egret.Bitmap;
     private jdImg:egret.Bitmap;
     private buImg:egret.Bitmap;
+    private xtqtImg:egret.Bitmap;
+    private xtjdImg:egret.Bitmap;
+    private xtbuImg:egret.Bitmap;
 
     private init(): void {
         let sky = this.createBitmapByName("green_png");
         this.addChild(sky);
         sky.width = GameApp.stageW;
         sky.height = GameApp.stageH;
+
+        var DsLabel = new egret.TextField();
+        DsLabel.textColor = 0xffffff;
+        DsLabel.x = GameApp.stageW/2-200;
+        DsLabel.y = GameApp.stageH/2-300;
+        DsLabel.text = "你的对手已经想好要出剪刀了";
+        DsLabel.size = 30;
+        this.addChild(DsLabel);
+        this.DsLabel = DsLabel;//这样写了才能在下面使用visible
+        // this.DsLabel.visible = true;
+
+        let xtqtImg = this.createBitmapByName("quantou_png");
+        this.addChild(xtqtImg);
+        xtqtImg.x =  GameApp.stageW/2-60;
+        xtqtImg.y = GameApp.stageH/2-300;
+        this.xtqtImg = xtqtImg;
+        this.xtqtImg.visible = false;
+
+        let xtjdImg = this.createBitmapByName("jiandao_png");
+        this.addChild(xtjdImg);
+        xtjdImg.x =  GameApp.stageW/2-60;
+        xtjdImg.y = GameApp.stageH/2-300;
+        this.xtjdImg = xtjdImg;
+        this.xtjdImg.visible = false;
+
+        let xtbuImg = this.createBitmapByName("bazhang_png");
+        this.addChild(xtbuImg);
+        xtbuImg.x =  GameApp.stageW/2-60;
+        xtbuImg.y = GameApp.stageH/2-300;
+        this.xtbuImg = xtbuImg;
+        this.xtbuImg.visible = false;
 
         // var btn:egret.DisplayObject = new egret.DisplayObject();
 		let qtImg = this.createBitmapByName("quantou_png");
@@ -77,23 +112,106 @@ class GameSceneLayer extends egret.Sprite{
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.qtImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(1);
     }
     private onjdButtonClick() {
         this.stLabel.visible = false;
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.jdImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(2);
     }
     private onbuButtonClick() {
         this.stLabel.visible = false;
         this.jdLabel.visible = false;
         this.buLabel.visible = false;
         this.buImg.visible = true;
+        this.DsLabel.visible = false;
+        this.getRandom(3);
     }
     private createBitmapByName(name: string): egret.Bitmap {
         let result = new egret.Bitmap();
         let texture: egret.Texture = RES.getRes(name);
         result.texture = texture;
         return result;
+    }
+
+    private getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+    }
+
+
+/**
+     * 创建分数栏
+     * create score container
+     */
+    private panel;
+    private strResuletLabel;
+    private createPanel(): void {
+        let panel = new eui.Panel();
+        panel.title = "Title";
+        panel.x = 100;
+        panel.y = 500;
+        this.addChild(panel);
+
+        var strResuletLabel = new egret.TextField();
+        strResuletLabel.textColor = 0x00CED1;
+        strResuletLabel.x = 100;
+        strResuletLabel.y = GameApp.stageH/2;
+        strResuletLabel.size = 30;
+        this.addChild(strResuletLabel);
+        this.strResuletLabel = strResuletLabel;//这样写了才能在下面使用visible
+        this.strResuletLabel.text = "平局";
+        strResuletLabel.touchEnabled = true;//bitmap设置之后才能触发点击
+        strResuletLabel.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+
+    }
+
+    private getRandom(result:number){
+        let num = this.getRandomIntInclusive(1,3);
+        console.log(num)
+        if (num===1){
+            this.xtqtImg.visible = true;
+        }else if (num===2){
+            this.xtjdImg.visible = true;
+        }else if(num === 3){
+            this.xtbuImg.visible = true;
+        }
+        this.createPanel();
+        if(result === 1){
+            if (num===1){
+
+            }else if (num===2){
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //赢了
+            }else if (num===3){
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //输了
+            }
+        } 
+        if(result === 2){
+            if (num===1){
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //输了
+            }else if (num===2){
+            }else if (num===3){
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //赢了
+            }
+        } 
+        if(result === 3){
+            if (num===1){
+                this.strResuletLabel.text = "赢了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //赢了
+            }else if (num===2){
+                this.strResuletLabel.text = "输了啊 啊啊啊 啊啊啊啊啊啊啊啊"
+                //输了
+            }else if (num===3){
+            }
+        }         
     }
 }
